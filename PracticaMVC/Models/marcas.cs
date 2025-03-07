ba002431f22f09fe3e0af1ead010f502767bc113
Eon-ts <aeonsitux@gmail.com>
2025-03-06 19:24:32 -0600

    Reglas de Model Marcas

--- PracticaMVC/Models/marcas.cs
+++ PracticaMVC/Models/marcas.cs
@@ -1,11 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace PracticaMVC.Models
 {
     public class marcas
     {
         [Key]
+        [DisplayName("Id De Marca")]
         public int id_marcas { get; set; }
+
+
+        [Required(ErrorMessage ="El nombre de la marca NO es opcional")]
+        [DisplayName("Nombre de la marca")]
         public string? nombre_marca { get; set; }
+
+
+        [DisplayName("Estado")]
+        [StringLength(1, ErrorMessage = "La cantidad maxima de caracteres valida es {1}")]
+        [Range(1,9, ErrorMessage ="Los valores aceptados son entre 1 y 9!")]
         public string? estados { get; set; }
     }
 }
